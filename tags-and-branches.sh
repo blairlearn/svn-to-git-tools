dab3c24db055ae35b69e375f032e519e338a9aab
Blair Learn <blairlearn@users.noreply.github.com>
2017-06-27 16:51:59 -0400

    Script to convert tags and branches

---
+++ tags-and-branches.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+echo 'Creating Branches.'
+for branch in `git branch -r | grep -v trunk | grep -v "origin/tags/" | sed 's/  origin\///'`; do
+    git branch $branch refs/remotes/origin/$branch ;
+done
+
+echo 'Creating Tags.'
+for tag in `git branch -r | grep "origin/tags/" | sed 's/  origin\/tags\///'`; do
+    git tag -a -m "Converting SVN tags." $tag refs/remotes/origin/tags/$tag
+done
